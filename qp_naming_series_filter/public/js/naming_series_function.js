e56e3c084d32da8ec6cef5c5ab06d65e4122785f
HendersonMentum <henderson.villegas@mentum.group>
2023-03-08 15:35:28 +0000

    add changes

---
+++ qp_naming_series_filter/public/js/naming_series_function.js
@@ -0,0 +1,49 @@
+
+function naming_series_filter(){
+
+    set_field_options("naming_series", [])
+
+    var naming_series_array = []
+
+    var naming_series_options = [];
+    
+    var naming_series_field = cur_frm.meta.fields.find(field => field.fieldname == "naming_series")
+    
+    if(naming_series_field){
+
+        naming_series_options = naming_series_field.options.split("\n")
+
+        if(naming_series_options){
+
+            frappe.db.get_list("Company", {fields:['name', 'abbr']}).then(
+                companies => {
+
+                    companies.forEach(company => {
+
+                        naming_series_options.forEach( (option, index, naming_series_options) => {
+
+                            var opt_com_abbr = option.split("-")
+                        
+                            if(opt_com_abbr[0].toUpperCase() == company.abbr.toUpperCase()){
+                                
+                                if(company.name == cur_frm.doc.company)
+                                    naming_series_array.push(option)
+
+                                naming_series_options.splice(index, 1)
+
+                            }
+                        })
+
+                    })
+                
+                    naming_series_array.push.apply(naming_series_array,naming_series_options)
+                
+                    cur_frm.set_value('naming_series', naming_series_array)
+                
+                    set_field_options("naming_series", naming_series_array)
+                }
+            )
+            
+        }
+    }
+}
